31f43685c5d5299cf863755dafe8318ecfdc468b
spukst3r <spukst3r@gmail.com>
2015-07-10 19:47:19 +0300

    Added classifiers

--- setup.py
+++ setup.py
@@ -17,5 +17,12 @@ setup(
         'Flask==0.10.1',
         'Werkzeug==0.10.4',
         'Flask-Cors==2.0.1'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
     ]
 )
